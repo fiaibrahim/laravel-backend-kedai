b0a3fd04f7b4634dcaa9a8f09ed986eb1a2998f5
fiaibrahim <selviaaibrahim123@gmail.com>
2024-10-14 10:38:31 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dashboard', function () {
+    return View('pages.dashboard', ['type_menu' => 'dashboard']);
+});
