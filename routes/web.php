60346fe668ac712c8c54d525bf537c42373a97ec
fiaibrahim <selviaaibrahim123@gmail.com>
2024-10-14 10:45:06 +0800

    konfigurasi auth

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
 
-Route::get('/dashboard', function () {
-    return View('pages.dashboard', ['type_menu' => 'dashboard']);
+
 });
